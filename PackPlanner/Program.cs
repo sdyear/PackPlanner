d67d60188e2fa636b536675eacfc89cd5f1f4aa0
sdyear <sdyear@gmail.com>
2023-03-26 22:19:12 +1300

    Added Item parsing and sorting

--- PackPlanner/Program.cs
+++ PackPlanner/Program.cs
@@ -1,19 +1,34 @@
 ﻿using System.Collections.Generic;
 
 internal class Program
-{    enum SortOrder
+{
+    enum SortOrder
     {
         NATURAL,
         LONG_TO_SHORT,
         SHORT_TO_LONG
     }
 
+    class Item
+    {
+        public Item(int id, int length, int quantity, float weight)
+        {
+            this.Id = id;
+            this.Length = length;
+            this.Quantity = quantity;
+            this.Weight = weight;
+        }
+        public int Id { get; set; }
+        public int Length { get; set; }
+        public int Quantity { get; set; }
+        public float Weight { get; set; }
+    }
     private static void Main(string[] args)
     {
         string? configString = Console.ReadLine();
         if (string.IsNullOrEmpty(configString)) throw new ArgumentException("config line required");
         string[] configArray = configString.Split(',');
-        if(configArray.Length != 3) throw new ArgumentException("4 config arguments are required");
+        if (configArray.Length != 3) throw new ArgumentException("4 config arguments are required");
 
         SortOrder order = SortOrder.NATURAL;
         int maxPieces = 0;
@@ -30,6 +45,29 @@ internal class Program
             return;
         }
 
-        Console.WriteLine(order.ToString() + ',' + maxPieces + ',' + maxWeight);
+        string? itemString;
+        List<Item> allItems = new List<Item>();
+        while (!string.IsNullOrEmpty(itemString = Console.ReadLine()))
+        {
+            string[] itemArray = itemString.Split(',');
+            if (itemArray.Length != 4) throw new ArgumentException("items must have 4 attribute values");
+            Item newItem;
+            try
+            {
+                newItem = new Item(int.Parse(itemArray[0]), int.Parse(itemArray[1]), int.Parse(itemArray[2]), float.Parse(itemArray[3]));
+                allItems.Add(newItem);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine($"Unable to parse item: '{itemString}'");
+                return;
+            }
+        }
+        if (order == SortOrder.SHORT_TO_LONG)
+            allItems.Sort((i1, i2) => i1.Length.CompareTo(i2.Length));
+        else if (order == SortOrder.LONG_TO_SHORT)
+            allItems.Sort((i1, i2) => i2.Length.CompareTo(i1.Length));
+
+
     }
 }
