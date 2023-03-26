a480c7527b656a4f1120cc11bde5efe458ddf1da
sdyear <sdyear@gmail.com>
2023-03-26 20:25:38 +1300

    Added config line parsing

--- PackPlanner/Program.cs
+++ PackPlanner/Program.cs
@@ -1,7 +1,35 @@
-﻿internal class Program
-{    
+﻿using System.Collections.Generic;
+
+internal class Program
+{    enum SortOrder
+    {
+        NATURAL,
+        LONG_TO_SHORT,
+        SHORT_TO_LONG
+    }
+
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        string? configString = Console.ReadLine();
+        if (string.IsNullOrEmpty(configString)) throw new ArgumentException("config line required");
+        string[] configArray = configString.Split(',');
+        if(configArray.Length != 3) throw new ArgumentException("4 config arguments are required");
+
+        SortOrder order = SortOrder.NATURAL;
+        int maxPieces = 0;
+        float maxWeight = 0;
+        try
+        {
+            order = (SortOrder)Enum.Parse(typeof(SortOrder), configArray[0]);
+            maxPieces = int.Parse(configArray[1]);
+            maxWeight = float.Parse(configArray[2]);
+        }
+        catch (Exception)
+        {
+            Console.WriteLine($"Unable to parse config: '{configString}'");
+            return;
+        }
+
+        Console.WriteLine(order.ToString() + ',' + maxPieces + ',' + maxWeight);
     }
 }
